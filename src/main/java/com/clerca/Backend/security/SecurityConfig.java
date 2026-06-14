0a17403408d99c98b2633fa78f8d5fda5598e4dc
Tharun Kumar <tharxn17@gmail.com>
2026-06-15 03:31:54 +0530

    solved cors

--- src/main/java/com/clerca/Backend/security/SecurityConfig.java
+++ src/main/java/com/clerca/Backend/security/SecurityConfig.java
@@ -62,7 +62,9 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("http://localhost:3000"));
+                config.setAllowedOrigins(List.of(
+                                "http://localhost:3000",
+                                "https://clerca-frontend.vercel.app"));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                 config.setAllowedHeaders(List.of(
                                 "Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"));
